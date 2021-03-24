b9ca533b58e448164a58fcc51e87e332185c87d7
adamoudad <adam.oudad@gmail.com>
2021-03-24 22:46:15 +0900

    Fix Piano e-Competition urls

--- dataset/scripts/ecomp_piano_downloader.sh
+++ dataset/scripts/ecomp_piano_downloader.sh
@@ -2,17 +2,17 @@
 # Scraper for Yamaha e-Piano Competition dataset
 [ ! "$1" ] && echo 'Error: please specify output dir' && exit
 dir=$1
-pages='http://www.piano-e-competition.com/ecompetition/midi_2002.asp
-http://www.piano-e-competition.com/ecompetition/midi_2004.asp
-http://www.piano-e-competition.com/ecompetition/midi_2006.asp
-http://www.piano-e-competition.com/ecompetition/midi_2008.asp
-http://www.piano-e-competition.com/ecompetition/midi_2009.asp
-http://www.piano-e-competition.com/ecompetition/midi_20011.asp
+pages='https://www.piano-e-competition.com/midi_2002.asp
+https://www.piano-e-competition.com/midi_2004.asp
+https://www.piano-e-competition.com/midi_2006.asp
+https://www.piano-e-competition.com/midi_2008.asp
+https://www.piano-e-competition.com/midi_2009.asp
+https://www.piano-e-competition.com/midi_20011.asp
 '
 mkdir -p $dir
 for page in $pages; do
     for midi in $(curl -s $page | egrep -i '[^"]+\.mid' -o | sed 's/^\/*/\//g'); do
-        echo "http://www.piano-e-competition.com$midi"
+        echo "https://www.piano-e-competition.com$midi"
     done
 done | wget -P $dir -i -
 cd $dir
